4b6dffef12f20731e2bd425aedf9ec65f13ba066
dtn1999 <tchekamboudanyls@gmail.com>
2023-02-02 17:14:52 +0100

    initial set up

---
+++ services/infrastucture-controller/src/main/java/io/codecm/infrastructure/controller/InfrastuctureControllerApplication.java
@@ -0,0 +1,13 @@
+package io.codecm.infrastructure.controller;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InfrastuctureControllerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(InfrastuctureControllerApplication.class, args);
+    }
+
+}
---
+++ services/infrastucture-controller/src/test/java/io/codecm/infrastructure/controller/InfrastuctureControllerApplicationTests.java
@@ -0,0 +1,13 @@
+package io.codecm.infrastructure.controller;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InfrastuctureControllerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
