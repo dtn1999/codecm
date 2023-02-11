8942cd80242fc52811474b00db883465e9b56746
dtn1999 <tchekamboudanyls@gmail.com>
2023-02-11 15:02:19 +0100

    initial configuration vm-configurer

--- vm-configurer/src/index.ts
+++ vm-configurer/src/index.ts
@@ -1,1 +1,27 @@
-console.log('TypeScript Eslint Prettier Starter Template!')
+import * as Hapi from '@hapi/hapi';
+
+const init = async () => {
+  const server = Hapi.server({
+    port: 3000,
+    host: 'localhost',
+  });
+
+  server.route({
+    method: 'GET',
+    path: '/',
+    handler: (request, h) => {
+      console.log(request, h);
+      return 'Hello World!';
+    },
+  });
+
+  await server.start();
+  console.log('Server running on %s', server.info.uri);
+};
+
+process.on('unhandledRejection', (err) => {
+  console.log(err);
+  process.exit(1);
+});
+
+init();
