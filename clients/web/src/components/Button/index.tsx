b5cd3d6df2dcb4cf8a583c1a43bc3dce57c2d700
dtn1999 <tchekamboudanyls@gmail.com>
2023-03-01 16:45:56 +0100

    update

--- clients/web/src/components/Button/index.tsx
+++ clients/web/src/components/Button/index.tsx
@@ -1,5 +1,61 @@
-import React from "react";
+import cn from "classnames";
+import type { ButtonHTMLAttributes } from "react";
+import React, { forwardRef, useRef } from "react";
+import { mergeRefs } from "react-merge-refs";
+import styles from "./Button.module.css";
+import { LoadingDots } from "@we/components/LoadingDots";
 
-export const Button = React.memo(() => (
-  <button className="bg-gray-800">button</button>
-));
+interface Props extends ButtonHTMLAttributes<HTMLButtonElement> {
+  variant?: "slim" | "flat";
+  active?: boolean;
+  width?: number;
+  loading?: boolean;
+  Component?: React.ComponentType;
+}
+
+export const Button = forwardRef<HTMLButtonElement, Props>(
+  (props, buttonRef) => {
+    const {
+      className,
+      variant = "flat",
+      children,
+      active,
+      width,
+      loading = false,
+      disabled = false,
+      style = {},
+      Component = "button",
+      ...rest
+    } = props;
+    const ref = useRef(null);
+    const rootClassName = cn(
+      {
+        [styles.slim || ""]: variant === "slim",
+        [styles.loading || ""]: loading,
+        [styles.disabled || ""]: disabled,
+      },
+      className
+    );
+    return (
+      <Component
+        aria-pressed={active}
+        data-variant={variant}
+        ref={mergeRefs([ref, buttonRef])}
+        className={rootClassName}
+        disabled={disabled}
+        style={{
+          width,
+          ...style,
+        }}
+        {...rest}
+      >
+        {children}
+        {loading && (
+          <i className="m-0 flex pl-2">
+            <LoadingDots />
+          </i>
+        )}
+      </Component>
+    );
+  }
+);
