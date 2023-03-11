4fed560a41362596b1c4a21b183e9e89d8448f1b
dtn1999 <tchekamboudanyls@gmail.com>
2023-03-11 15:29:13 +0100

    add playgrounds hooks"

--- clients/web/src/features/resources/components/Grids/PlaygroundsGrid.tsx
+++ clients/web/src/features/resources/components/Grids/PlaygroundsGrid.tsx
@@ -1,35 +1,48 @@
-import { trpc } from "@we/utils/api";
+import { Spinner } from "@we/components";
+import { Playground } from "@we/types/schemas";
 import React from "react";
 import { PlaygroundCard } from "../Cards/PlaygroundCard";
 
-export const PlaygroundsGrid: React.FC = React.memo(() => {
-  const { data, error } = trpc.playgrounds.getAll.useQuery();
-  const { mutateAsync } = trpc.playgrounds.delete.useMutation();
-  const handleDeleteClick = async (playgroundId: number) => {
-    const { data } = await mutateAsync({ playgroundId });
-    console.log(data);
-  };
-  console.log(data, error);
-  return !data || data.playgrounds.length === 0 ? (
-    <div className="mx-auto w-full max-w-screen-2xl py-5">
-      <div>
-        <ul className="space-y-4">
-          <h6 className="text-center text-sm italic text-gray-500">
-            No playgrounds to show
-          </h6>
-        </ul>
+interface Props {
+  playgrounds: Playground[];
+  handleDelete: (playgroundId: number) => void;
+  handleEdit: () => void;
+  isFetching: boolean;
+}
+export const PlaygroundsGrid: React.FC<Props> = React.memo(
+  ({ playgrounds, handleDelete, handleEdit, isFetching }) => {
+    return isFetching ? (
+      <div className="flex h-full w-full items-center justify-center py-10">
+        <Spinner />
       </div>
-    </div>
-  ) : (
-    <div className="mt-4 grid grid-cols-1 gap-4">
-      {data?.playgrounds.map((playground) => (
-        <PlaygroundCard
-          key={playground.id}
-          handleDelete={handleDeleteClick}
-          handleEdit={() => {}}
-          {...playground}
-        />
-      ))}
-    </div>
-  );
-});
+    ) : (
+      <React.Fragment>
+        {/* no playgrounds */}
+        {playgrounds.length === 0 && (
+          <div className="mx-auto w-full max-w-screen-2xl py-5">
+            <div>
+              <ul className="space-y-4">
+                <h6 className="text-center text-sm italic text-gray-500">
+                  No playgrounds to show
+                </h6>
+              </ul>
+            </div>
+          </div>
+        )}
+        {/* playgrounds grid */}
+        {playgrounds.length > 0 && (
+          <div className="mt-4 grid grid-cols-1 gap-4">
+            {playgrounds.map((playground) => (
+              <PlaygroundCard
+                key={playground.id}
+                handleDelete={handleDelete}
+                handleEdit={handleEdit}
+                {...playground}
+              />
+            ))}
+          </div>
+        )}
+      </React.Fragment>
+    );
+  }
+);
---
+++ clients/web/src/features/resources/hooks/usePlayground.tsx
@@ -0,0 +1,36 @@
+import React from "react";
+import { useSession } from "next-auth/react";
+import { Playground } from "@we/types/schemas";
+import { trpc } from "@we/utils/api";
+
+export const usePlaygrounds = () => {
+  const [playgrounds, setPlaygrounds] = React.useState<Playground[]>([]);
+  const [fetchPlaygroundEnabled, setFetchPlaygroundEnabled] = React.useState(false)
+  const { data: sessionData } = useSession();
+  const deletePlayground = React.useCallback(async (id: string) => {}, []);
+  const editTemplate = React.useCallback(async (id: string) => {}, []);
+  const { data, isLoading: fetchingPlaygrounds } = trpc.playgrounds.getAll.useQuery(undefined, {
+    enabled: fetchPlaygroundEnabled,
+    placeholderData: [],
+  });
+
+  React.useEffect(() => {
+    if (!sessionData || !sessionData.user) {
+        setPlaygrounds([]);
+    } else {
+    }
+  }, [sessionData]);
+
+  React.useEffect(() => {
+    if(data && data.playgrounds) {
+        setPlaygrounds(data.playgrounds)
+    }
+  }, [data])
+
+  return {
+    fetchingPlaygrounds,
+    playgrounds,
+    deletePlayground,
+    editTemplate,
+  };
+};
